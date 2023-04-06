d14b687f848d3aafa18c75d78490741c82cbe5ed
fatihaltindis <fthaltindis@gmail.com>
2023-04-06 17:04:51 +0200

    folder

--- test.jl
+++ test.jl
@@ -33,7 +33,7 @@ dt = false;
 
 filepath = "G:\\Mon Drive\\PhD\\Codes\\julia_codes\\group-learning\\Group-Learning-BCI\\ERP Analysis\\DataBases\\P300"
 
-selected_files, subject_list = select_subjects(dbList; n_of_subject = n_of_subject,
+selected_files, subject_list = selectSubjects(dbList; n_of_subject = n_of_subject,
                                                selection_rule = selection_rule, turn = :none,
                                                filepath = filepath);
 
@@ -44,6 +44,8 @@ temp_file = selected_files[1];
 o=readNY(temp_file;
          bandpass=bandpass, upperLimit=artefact_rejection);
 TAindex = findfirst(isequal("Target"), o.clabels);
+
+include("dataTransformation.jl")
 
 for split_ratio in 10:10:90
     for n_splits in (1:10)
@@ -77,9 +79,9 @@ for split_ratio in 10:10:90
 end
 
 # TEST CODE for chekcing the overlap between consecutive train or test splits
-include("dataTransformation.jl")
+
 for n_splits in (1:10)
-    train_splits_no, test_splits_no, y_train, y_test = get_splits(o.mark; 
+    train_splits_no, test_splits_no, y_train, y_test = getSplits(o.mark; 
                     n_splits = n_splits,
                     split_ratio = split_ratio,
                     return_type = :trial_no,
@@ -95,6 +97,10 @@ for n_splits in (1:10)
     display(parity_check)
 end
 
+
+include("covarianceEstimation.jl")
+include("erpTools.jl")
+include("tangentSpaceVectors.jl")
 all_clabels = [] 	# Class labels of all subjects
 train_vecs = Vector{Matrix{Float64}}[]
 test_vecs = Vector{Matrix{Float64}}[]
@@ -105,24 +111,32 @@ for (n,temp_file) ∈ enumerate(selected_files)
              bandpass=bandpass, upperLimit=artefact_rejection);
     TAindex = findfirst(isequal("Target"), o.clabels);
     # Split the data
-    train_splits, _ = get_splits(o.mark; 
+    train_splits, _ = getSplits(o.mark; 
             n_splits = n_splits,
             split_ratio = split_ratio,
             return_type = :index,
             random_state = random_state);
-    train_splits_no, test_splits_no, y_train, y_test = get_splits(o.mark; 
+    train_splits_no, test_splits_no, y_train, y_test = getSplits(o.mark; 
             n_splits = n_splits,
             split_ratio = split_ratio,
             return_type = :trial_no,
             random_state = random_state);
     # ERP Prototype estimation
-    Y = erp_prototype(o.X, o.wl, train_splits, TAindex;
-                      overlapping = overlapping,
-                      weights = ERP_weight,
-                      PCA = PCA_dim);
+    Y = createPrototype(o.X, o.wl, train_splits, TAindex;
+                        overlapping = overlapping,
+                        weights = ERP_weight,
+                        PCA_dim = PCA_dim);
     # Covariance Estimation
-    train_covs, test_covs = estimate_cov_mat(o.trials, train_splits_no, test_splits_no, Y;
+    train_covs, test_covs = estimateCov(o.trials, train_splits_no, test_splits_no, Y;
                             det_normalization = det_normalization, estimator = :lw);
+
+
+    # Y = createPrototype(o.X, o.wl, o.mark, TAindex;
+    #                     overlapping = overlapping,
+    #                     weights = ERP_weight,
+    #                     PCA_dim = PCA_dim);
+    # covs = estimateCov(o.trials, Y; det_normalization = det_normalization, estimator = :lw);
+
     if dt
         # Avoid any mismatch of upper/lower case between channel names
         # This part is for correcting a small bug in header data of bi2014b data !!!
@@ -130,11 +144,11 @@ for (n,temp_file) ∈ enumerate(selected_files)
         if o.db == "bi2014b"
             deleteat!(ch_sub, findall(x->x=="afz",ch_sub))
         end
-        train_tsvec, test_tsvec = tangent_vectors(train_covs, test_covs, 1:o.ne, paradigm; 
+        train_tsvec, test_tsvec = tangentVectors(train_covs, test_covs, 1:o.ne, paradigm; 
                 transpose=false, dt = dt, ch_union = ch_union, ch_sub = ch_sub,
                 PCA_dim = PCA_dim);
     else
-        train_tsvec, test_tsvec = tangent_vectors(train_covs, test_covs, 1:o.ne, paradigm; 
+        train_tsvec, test_tsvec = tangentVectors(train_covs, test_covs, 1:o.ne, paradigm; 
                 transpose=false)
     end
     # append data
@@ -144,7 +158,7 @@ for (n,temp_file) ∈ enumerate(selected_files)
     push!(test_labels, y_test)
     append!(all_clabels,[o.clabels])
 end
-train_vecs, test_vecs = swap_dims(train_vecs, test_vecs);
+train_vecs, test_vecs = swapDims(train_vecs, test_vecs);
 
 # Subject-wise learning
 sw_acc, sw_std = sw_training(train_vecs, test_vecs, train_labels, test_labels; classifier = :LinearSVC);
@@ -158,12 +172,13 @@ include("dbInfo.jl")
 dbList = ["bi2012", "bi2014a", "bi2014b", "bi2015a", "BNCI2014008", "BNCI2015003"];
 n_of_subject = 0;
 random_state = nothing;
-selection_rule = :thre;
+selection_rule = :none;
 turn = :local; 
 for db in dbList
     (n_of_subject >= dbSubjectCount[db]) || n_of_subject==0 ? temps = dbSubjectCount[db] : temps = n_of_subject;
     selected_files, subject_list = select_subjects(db; n_of_subject = n_of_subject, threshold = 0.2,
-                                                   selection_rule = selection_rule, turn = :none);
+                                                   selection_rule = selection_rule, turn = :none,
+                                                   filepath = filepath);
     c = size(selected_files,1)
     if c == temps * dbSessionCount[db]
         @info("Database:    $(db)   >>>     Filename list count matches")
